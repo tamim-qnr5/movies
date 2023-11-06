2beb311dbcb1f5b399ac27b528d951c524c5b69a
nancy khaled <nancykhaledn905@gmail.com>
2023-11-06 07:02:24 +0200

    update popular screen.

--- lib/view/main_movies/popular/popular_screen.dart
+++ lib/view/main_movies/popular/popular_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -23,7 +23,7 @@ class MainScreen extends StatelessWidget {
             ? ListView(
                 children: [
                   SizedBox(
-                    height: MediaQuery.of(context).size.height*0.36,
+                    height: MediaQuery.of(context).size.height * 0.36,
                     child: InkWell(
                       onTap: () {
                         Navigator.push(
@@ -32,88 +32,115 @@ class MainScreen extends StatelessWidget {
                                 builder: (context) => MovieDetailsView(
                                     provider.results[12].id!.toInt())));
                       },
-                      child: Stack(
-                        alignment: Alignment.bottomLeft,
-                        children: [
-                          Column(
-                            children: [
-                              SizedBox(
-                                height: MediaQuery.of(context).size.height*0.29,
-                                child: FadeInImage.assetNetwork(
-                                  width: double.infinity,
-                                  fit: BoxFit.cover,
-                                  placeholder: 'assets/loadingPicture.jpg',
-                                  image:
-                                      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2'
-                                      '${provider.results[12].backdropPath ?? ''}',
-                                  imageErrorBuilder: (x,c,v)=>Image.asset('assets/loadingPicture.jpg',fit: BoxFit.fill,),
-                                ),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
+                      child: CarouselSlider.builder(
+                          itemCount: provider.results.length,
+                          options: CarouselOptions(
+                            height: 350,
+                            enableInfiniteScroll: true,
+                            enlargeCenterPage: true,
+                            viewportFraction: 1,
+                            pageSnapping: true,
+                          ),
+                          itemBuilder: (context, itemIndex, pageViewIndex) {
+                            return Stack(
+                              alignment: Alignment.bottomLeft,
+                              children: [
+                                Column(
                                   children: [
-                                    const SizedBox(
-                                      height: 5,
-                                    ),
-                                    Padding(
-                                      padding:
-                                          const EdgeInsets.only(left: 100.0),
-                                      child: Text(
-                                        provider.results[12].title ?? '',
-                                        style: const TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 14,
-                                        ),
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(top: 5.0),
-                                      child: Text(
-                                        provider.results[12].releaseDate ?? '',
-                                        style: const TextStyle(
-                                          color: Color(0xffB5B4B4),
-                                          fontSize: 10,
+                                    SizedBox(
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.29,
+                                      child: FadeInImage.assetNetwork(
+                                        width: double.infinity,
+                                        fit: BoxFit.cover,
+                                        placeholder:
+                                            'assets/loadingPicture.jpg',
+                                        image:
+                                            'https://www.themoviedb.org/t/p/w600_and_h900_bestv2'
+                                            '${provider.results[itemIndex].backdropPath ?? ''}',
+                                        imageErrorBuilder: (x, c, v) =>
+                                            Image.asset(
+                                          'assets/loadingPicture.jpg',
+                                          fit: BoxFit.fill,
                                         ),
                                       ),
                                     ),
+                                    Expanded(
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 100.0),
+                                            child: Text(
+                                              provider.results[itemIndex]
+                                                      .title ??
+                                                  '',
+                                              style: const TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 14,
+                                              ),
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding:
+                                                const EdgeInsets.only(top: 5.0),
+                                            child: Text(
+                                              provider.results[itemIndex]
+                                                      .releaseDate ??
+                                                  '',
+                                              style: const TextStyle(
+                                                color: Color(0xffB5B4B4),
+                                                fontSize: 10,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    )
                                   ],
                                 ),
-                              )
-                            ],
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 8.0),
-                            child: Container(
-                              clipBehavior: Clip.antiAlias,
-                              height: MediaQuery.of(context).size.height*0.20,
-                              width: MediaQuery.of(context).size.width*0.34,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                  border: Border.all(
-                                      color: Colors.white, width: 2)),
-                              child: FadeInImage.assetNetwork(
-                                fit: BoxFit.fill,
-                                placeholder: 'assets/loadingPicture.jpg',
-                                image:
-                                    'https://www.themoviedb.org/t/p/w600_and_h900_bestv2${provider.results[12].backdropPath ?? ''}',
-                                imageErrorBuilder:
-                                    (x,c,v)=>Image.asset('assets/loadingPicture.jpg',fit: BoxFit.fill,),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 8.0),
+                                  child: Container(
+                                    clipBehavior: Clip.antiAlias,
+                                    height: MediaQuery.of(context).size.height *
+                                        0.20,
+                                    width: MediaQuery.of(context).size.width *
+                                        0.34,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        border: Border.all(
+                                            color: Colors.white, width: 2)),
+                                    child: FadeInImage.assetNetwork(
+                                      fit: BoxFit.fill,
+                                      placeholder: 'assets/loadingPicture.jpg',
+                                      image:
+                                          'https://www.themoviedb.org/t/p/w600_and_h900_bestv2${provider.results[itemIndex].backdropPath ?? ''}',
+                                      imageErrorBuilder: (x, c, v) =>
+                                          Image.asset(
+                                        'assets/loadingPicture.jpg',
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            );
+                          }),
                     ),
                   ),
                   const SizedBox(
                     height: 10,
                   ),
-
                   Container(
-                    height: MediaQuery.of(context).size.height*0.06,
+                    height: MediaQuery.of(context).size.height * 0.06,
                     width: double.infinity,
                     alignment: Alignment.centerLeft,
                     margin: const EdgeInsets.only(left: 10),
@@ -133,16 +160,15 @@ class MainScreen extends StatelessWidget {
                     margin: const EdgeInsets.only(left: 10),
                     decoration: const BoxDecoration(color: Color(0xff282A28)),
                     child: SizedBox(
-                      height: MediaQuery.of(context).size.height*0.2,
+                      height: MediaQuery.of(context).size.height * 0.2,
                       child: ReleaseScreen(),
                     ),
                   ),
-
                   const SizedBox(
                     height: 5,
                   ),
                   Container(
-                    height: MediaQuery.of(context).size.height*0.06,
+                    height: MediaQuery.of(context).size.height * 0.06,
                     width: double.infinity,
                     alignment: Alignment.centerLeft,
                     margin: const EdgeInsets.only(left: 10),
@@ -162,16 +188,18 @@ class MainScreen extends StatelessWidget {
                     margin: const EdgeInsets.only(left: 10),
                     decoration: const BoxDecoration(color: Color(0xff282A28)),
                     child: SizedBox(
-                        height: MediaQuery.of(context).size.height*0.3,
-                      child: TopRatedView(check: true,),
+                      height: MediaQuery.of(context).size.height * 0.3,
+                      child: TopRatedView(
+                        check: true,
+                      ),
                     ),
                   ),
                 ],
               )
             : const Text(
-              'No data',
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            );
+                'No data',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              );
       }
     });
   }
